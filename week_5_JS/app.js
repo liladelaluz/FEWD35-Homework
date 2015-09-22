323c2ad48045de584260baad64aadd229e383659
liladelaluz <liladelaluz@yahoo.com>
2015-09-21 21:19:26 -0700

    Homework_Bootstrap
    Homework for week 4

---
+++ week_5_JS/app.js
@@ -0,0 +1,55 @@
+// //alert("Hello World!");
+// //alert(2+3);
+
+// // var num1=2;
+// // var num2=3;
+
+// // alert(num1 + num2);
+
+// // var Lila =["Lila", "Artist", "SF Base"];
+
+// // // alert(Lila[1]);
+
+// // alert(Lila[0] + " " + Lila[2]);
+
+// function sayHello() {
+// 	alert("Hello!");
+// }
+
+// sayHello();
+
+// function addTwo(num1, num2) {
+// 	return num1 + num2;
+// }
+
+// var result1= addTwo(1, 2);
+
+// var result1= addTwo(4, 5);
+
+// // alert(result1);
+// // alert(result2);
+
+// // console.log(result1);
+
+// for (var i = 0; i < 20; i++) {
+// 	console.log(i);
+// }
+
+var name = "Lila";
+if (name === "Lila") {
+	alert("Hey Lila!");
+} else {
+	alert("I don't know you");
+}
+
+var age = 30;
+
+if (age<30) {
+	alert("You are less than 30");
+}
+else if (age === 30) {
+	alert ("You are 30");
+} else {
+	alert("You are over 30");
+}
+
